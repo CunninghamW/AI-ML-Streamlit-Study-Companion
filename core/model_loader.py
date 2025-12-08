07bba4531470cb6bb49c366b4baa937d132d2cf6
CunninghamW <wcunni14@wgu.edu>
2025-12-08 17:22:52 -0500

    Project Structure

---
+++ core/model_loader.py
@@ -0,0 +1,21 @@
+import os
+import joblib
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.neighbors import NearestNeighbors
+
+class ModelLoader:
+    def __init__(self, csv_path):
+        pass
+
+    def _ensure_model_dir():
+        pass
+
+    def load_or_train_models():
+        pass
+
+    def vectorize_question(question):
+        pass
+
+    def get_knn_model():
+        pass
