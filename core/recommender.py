6d28582421458a4d4278d59ef5899e230f29803f
CunninghamW <wcunni14@wgu.edu>
2025-12-11 10:30:09 -0500

    Completed Recommender class, with method get_similar_questions

--- core/recommender.py
+++ core/recommender.py
@@ -3,7 +3,20 @@
 
 class Recommender:
     def __init__(self, model_loader: ModelLoader):
-        pass
+        self.model_loader = model_loader
 
-    def get_smaller_questions(question_or_id, top_k=5):
-        pass
+
+    def get_similar_questions(self, question_text, top_k=5):
+        """
+        Docstring for get_similar_questions
+        
+        Return the top_k questions most similar to the given question_text.
+        uses the TF-IDF vectorizer and the KNN model from ModelLoader.
+        """
+        
+        # ask the model loader to get similar questions
+        all_similar = self.model_loader.find_similar(question_text)
+
+        # return only the top_k results
+
+        return all_similar[:top_k]
